882bcb503166ebc3accf527ef9658dc070a8f63e
Osanda Deshan <osanda.deshan@gmail.com>
2023-03-15 10:47:02 +0800

    Updated tests and moved jest config to jest.config.json

--- src/__test__/Counter.test.js
+++ src/__test__/Counter.test.js
@@ -3,15 +3,17 @@ import Counter from "../Counter";
 
 const setup = () => render(<Counter />);
 
-describe("Increment counter should increase the number when user clicks on it", () => {
-    it("Counter should start with 0", () => {
+describe("Counter should start with 0", () => {
+    it("Initial value of the counter should be 0", () => {
         setup();
         const counter = screen.getByTestId("counter");
         
         expect(counter).toHaveTextContent("0");
     });
+});
 
-    it("Counter should increase the value to 1 when user clicks one time on increment counter", () => {
+describe("Increment button should increase the number when user clicks on it", () => {
+    it("Counter should increase the value to 1 when user clicks one time on increment button", () => {
         setup();
         const counter = screen.getByTestId("counter");
         const incrementBtn = screen.getByTestId("increment");
@@ -21,16 +23,41 @@ describe("Increment counter should increase the number when user clicks on it", 
         expect(counter).toHaveTextContent("1");
     });
 
-    it("Counter should decrease the value to 1 when user clicks one time on decrement counter at the value of 2", () => {
+    it("Counter should increase the value to 2 when user clicks two times on increment button", () => {
+        setup();
+        const counter = screen.getByTestId("counter");
+        const incrementBtn = screen.getByTestId("increment");
+
+        fireEvent.click(incrementBtn);
+        fireEvent.click(incrementBtn);
+
+        expect(counter).toHaveTextContent("2");
+    });
+});
+
+describe("Decrement button should decrease the number when user clicks on it", () => {
+    it("Counter should decrease the value to 1 when user clicks one time on decrement button at the value of 2", () => {
         setup();
         const counter = screen.getByTestId("counter");
         const incrementBtn = screen.getByTestId("increment");
         const decrementBtn = screen.getByTestId("decrement");
+        fireEvent.click(incrementBtn);
+        fireEvent.click(incrementBtn);
 
-        fireEvent.click(incrementBtn);
-        fireEvent.click(incrementBtn);
         fireEvent.click(decrementBtn);
 
         expect(counter).toHaveTextContent("1");
+    });
+
+    it("Counter should decrease the value to 0 when user clicks one time on decrement button at the value of 1", () => {
+        setup();
+        const counter = screen.getByTestId("counter");
+        const incrementBtn = screen.getByTestId("increment");
+        const decrementBtn = screen.getByTestId("decrement");
+        fireEvent.click(incrementBtn);
+        
+        fireEvent.click(decrementBtn);
+
+        expect(counter).toHaveTextContent("0");
     });
 });
